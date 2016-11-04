1bc840f7acfe7ef17efc4f28bc21763993a774ec
Ensky Lin <enskylin@gmail.com>
2016-11-05 00:22:43 +0800

    bump version

--- taiga_contrib_ldap_auth/__init__.py
+++ taiga_contrib_ldap_auth/__init__.py
@@ -14,6 +14,6 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-__version__ = (0, 1, 0)
+__version__ = (0, 1, 1)
 
 default_app_config = "taiga_contrib_ldap_auth.apps.TaigaContribLDAPAuthAppConfig"
